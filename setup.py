a36d85567dabc7ae070567e2c4365492da32b2ef
MyDen-cf <64523847+myden5279@users.noreply.github.com>
2020-11-16 11:08:14 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+setup(
+  name = 'mymath',
+  packages = ['mymath'],
+  version = '1.0',
+  license='MIT',
+  description = 'open source advanced calculator',
+  author = 'MyDen',
+  author_email = 'myden001002@gmail.com',
+  url = 'https://github.com/myden5279/mymath.git',
+  download_url = 'https://github.com/myden5279/mymath/archive/v1.0.tar.gz',
+  keywords = ['calculator', 'maths', 'mymath'],
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+  ],
+)
